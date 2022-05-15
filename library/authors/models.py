2f52ed9dfd2518b38524bb089fc316f6eef0b652
max31ru12 <maxevg72@gmail.com>
2022-05-16 00:32:27 +0300

    lesson_1 on main branch

---
+++ library/authors/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Author(models.Model):
+    first_name = models.CharField("Имя", max_length=64)
+    last_name = models.CharField("Фамилия", max_length=64)
+    birthday_year = models.PositiveIntegerField()
