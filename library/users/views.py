4eaf1289e53c10920bea0418b05860138f6a766e
max31ru12 <maxevg72@gmail.com>
2022-06-12 13:35:36 -0700

    added limitoffsetpagination for model User

--- library/users/views.py
+++ library/users/views.py
@@ -2,8 +2,15 @@ from rest_framework.viewsets import ModelViewSet
 
 from .models import User
 from .serializers import UserModelSerializer
+from rest_framework.pagination import LimitOffsetPagination
+class UserLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 100 
+
 
 
 class UserModelViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserModelSerializer
+    pagination_class = UserLimitOffsetPagination
+
+
